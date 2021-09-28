a4f5dc7539500b99d0ccf014695617760c0e4a59
hwkmov <mov0702@gmail.com>
2021-09-28 20:47:53 +0900

    tagを追加 新着記事を追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\Tag;
 
 class PostController extends Controller
 {
@@ -22,14 +23,23 @@ class PostController extends Controller
         $items = Category::where('slug', $category_slug)->first()->posts;
         $categories = Category::withCount('posts')->get();
 
-        return view('post_list', ['items' => $items, 'categories' => $categories]);
+        return view('post_list', ['keyword'=> $category_slug, 'items' => $items, 'categories' => $categories]);
+    }
+
+    public function tag(Request $request, $tag_slug) 
+    {
+        $items = Tag::where('slug', $tag_slug)->first()->posts;
+        $categories = Category::withCount('posts')->get();
+
+        return view('post_list', ['keyword'=> $tag_slug, 'items' => $items, 'categories' => $categories]);
     }
 
     public function show(Request $request, $post_slug)
     {
         $item = Post::where('slug', $post_slug)->first();
         $categories = Category::withCount('posts')->get();
+        $new_posts = Post::orderBy('created_at', 'desc')->take(3)->get();
 
-        return view('post', ['item' => $item, 'categories' => $categories]);
+        return view('post', ['item' => $item, 'new_posts'=>$new_posts, 'categories' => $categories]);
     }
 }
