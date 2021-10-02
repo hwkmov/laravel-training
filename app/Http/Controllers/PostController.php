248fc7e6421c5327d714c87ace21bbfeecc1a26d
hwkmov <mov0702@gmail.com>
2021-10-02 15:23:39 +0900

    新着投稿表示を修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,24 +14,27 @@ class PostController extends Controller
     {
         $items = Post::all();
         $categories = Category::withCount('posts')->get();
+        $new_posts = Post::orderBy('created_at', 'desc')->take(3)->get();
 
-        return view('post', ['items' => $items, 'categories' => $categories]);
+        return view('post', ['items' => $items, 'new_posts'=>$new_posts, 'categories' => $categories]);
     }
 
     public function category(Request $request, $category_slug) 
     {
         $items = Category::where('slug', $category_slug)->first()->posts;
         $categories = Category::withCount('posts')->get();
+        $new_posts = Post::orderBy('created_at', 'desc')->take(3)->get();
 
-        return view('post_list', ['keyword'=> $category_slug, 'items' => $items, 'categories' => $categories]);
+        return view('post_list', ['keyword'=> $category_slug, 'items' => $items, 'new_posts'=>$new_posts, 'categories' => $categories]);
     }
 
     public function tag(Request $request, $tag_slug) 
     {
         $items = Tag::where('slug', $tag_slug)->first()->posts;
         $categories = Category::withCount('posts')->get();
+        $new_posts = Post::orderBy('created_at', 'desc')->take(3)->get();
 
-        return view('post_list', ['keyword'=> $tag_slug, 'items' => $items, 'categories' => $categories]);
+        return view('post_list', ['keyword'=> $tag_slug, 'items' => $items, 'new_posts'=>$new_posts, 'categories' => $categories]);
     }
 
     public function show(Request $request, $post_slug)
